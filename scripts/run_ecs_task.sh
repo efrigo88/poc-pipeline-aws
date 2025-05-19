b7fd662b2928c0f30f64b772b735f3280332cd07
Emiliano Frigo <emif.iron@gmail.com>
2025-05-19 10:24:35 +0200

    modify scripts

--- scripts/run_ecs_task.sh
+++ scripts/run_ecs_task.sh
@@ -7,10 +7,8 @@ set -e
 source .env
 
 # Export AWS credentials from .env
-# Export AWS credentials from .env
 export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
-export AWS_SESSION_TOKEN
 export AWS_DEFAULT_REGION
 
 # Configuration
